41296ca78d02b77e4d75ca6ad4461afbdd229239
EltonJCS <80626909+EltonJCS@users.noreply.github.com>
2023-12-23 23:47:22 -0300

    style(layout): update CSS styles for improved responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body
-        className={`${inter.className}  relative overscroll-none bg-slate-200 text-slate-950`}
+        className={`${inter.className}  relative overscroll-none bg-slate-200 pt-28 text-slate-950 sm:pt-36`}
       >
         <div className="absolute right-0 top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-red-100 blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-blue-100 blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
