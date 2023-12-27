21893f069604cb9f5d5f036bf6b566e867c513c0
EltonJCS <80626909+EltonJCS@users.noreply.github.com>
2023-12-27 19:05:07 -0300

    feat(section-divider): implement SectionDivider component

---
+++ src/components/section-divider.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { motion } from "framer-motion";
+
+const SectionDivider = () => {
+  return (
+    <motion.div
+      className="my-24 hidden h-16 w-1 rounded-full bg-slate-400 sm:block"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.25 }}
+    ></motion.div>
+  );
+};
+
+export default SectionDivider;
