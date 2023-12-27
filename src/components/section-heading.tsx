7a2dfc22c9859c5f7dc6f25e14724e496c79a19c
EltonJCS <80626909+EltonJCS@users.noreply.github.com>
2023-12-27 19:06:01 -0300

    feat(section-heading): implement SectionHeading component

---
+++ src/components/section-heading.tsx
@@ -0,0 +1,9 @@
+type SectionHeadingProps = {
+  title: string;
+};
+
+const SectionHeading = ({ title }: SectionHeadingProps) => {
+  return <h2 className="mb-8 text-3xl font-medium capitalize">{title}</h2>;
+};
+
+export default SectionHeading;
