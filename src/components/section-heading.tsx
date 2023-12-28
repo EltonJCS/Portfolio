be0ae3ccaf8257787dc928256fe24d3e370c2cc0
EltonJCS <80626909+EltonJCS@users.noreply.github.com>
2023-12-28 17:54:56 -0300

    style(section-heading): apply text-center class

--- src/components/section-heading.tsx
+++ src/components/section-heading.tsx
@@ -3,7 +3,11 @@ type SectionHeadingProps = {
 };
 
 const SectionHeading = ({ title }: SectionHeadingProps) => {
-  return <h2 className="mb-8 text-3xl font-medium capitalize">{title}</h2>;
+  return (
+    <h2 className="mb-8 text-center text-3xl font-medium capitalize">
+      {title}
+    </h2>
+  );
 };
 
 export default SectionHeading;
