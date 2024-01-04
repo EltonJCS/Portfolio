3c74b97dcd9b3b9ff2eb8bc652dd58124ba4ae79
EltonJCS <80626909+EltonJCS@users.noreply.github.com>
2024-01-04 11:22:26 -0300

    refactor(types): extract SectionName type into types.js

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -3,9 +3,7 @@ import { useEffect } from "react";
 
 import { useActiveSectionContext } from "../context/active-section-context";
 
-import { links } from "./data";
-
-type SectionName = (typeof links)[number]["name"];
+import type { SectionName } from "./types";
 
 export function useSectionInView(sectionName: SectionName, threshold = 0.8) {
   const { ref, inView } = useInView({ threshold });
