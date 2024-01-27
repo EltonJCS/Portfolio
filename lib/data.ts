d522a403148964fafe6f4c3f8617abf79ab2fdbc
EltonJCS <80626909+EltonJCS@users.noreply.github.com>
2024-01-26 23:43:22 -0300

    refactor(projects): move projectsData to data file

--- lib/data.ts
+++ lib/data.ts
@@ -1,3 +1,7 @@
+import projectModimisa from "../public/images/project-Modimisa.jpg";
+import projectMyLinks from "../public/images/project-MyLinks.jpg";
+import projectJavaRestAPI from "../public/images/project-java_restAPI.jpg";
+
 export const links = [
   {
     name: "Início",
@@ -22,5 +26,42 @@ export const links = [
   {
     name: "Contato",
     hash: "#contato",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Modimisa",
+    description:
+      "Site que possibilita a customização de uma camisa com visualização em 3D.",
+    tags: [
+      "React",
+      "Tailwind",
+      "Three.js",
+      "Framer Motion",
+      "Valtio",
+      "React Color",
+    ],
+    imageURL: projectModimisa,
+    demo: "https://eltonjcs.github.io/modimisa",
+    repository: "https://github.com/EltonJCS/modimisa",
+  },
+  {
+    title: "MyLinks",
+    description:
+      "Site agregador de links que permite o compartilhamento de redes sociais, sites e outros links importantes em um único lugar.",
+    tags: ["Java", "Spark"],
+    imageURL: projectMyLinks,
+    demo: "https://eltonjcs.github.io/MyLinks",
+    repository: "https://github.com/EltonJCS/MyLinks",
+  },
+  {
+    title: "java_restAPI",
+    description:
+      "API REST em Java com funcionalidades de CRUD, utilizando o microframework Spark.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageURL: projectJavaRestAPI,
+    demo: "",
+    repository: "https://github.com/EltonJCS/java_restAPI_clientCRUD",
   },
 ] as const;
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,50 +1,11 @@
 "use client";
-import { Fragment, useEffect } from "react";
+import { Fragment } from "react";
 
-import projectModimisa from "../../public/images/project-Modimisa.jpg";
-import projectMyLinks from "../../public/images/project-MyLinks.jpg";
-import projectJavaRestAPI from "../../public/images/project-java_restAPI.jpg";
+import { projectsData } from "../../lib/data";
 
 import SectionHeading from "./section-heading";
 import Project from "./project";
 import { useSectionInView } from "../../lib/hooks";
-
-const projectsData = [
-  {
-    title: "Modimisa",
-    description:
-      "Site que possibilita a customização de uma camisa com visualização em 3D.",
-    tags: [
-      "React",
-      "Tailwind",
-      "Three.js",
-      "Framer Motion",
-      "Valtio",
-      "React Color",
-    ],
-    imageURL: projectModimisa,
-    demo: "https://eltonjcs.github.io/modimisa",
-    repository: "https://github.com/EltonJCS/modimisa",
-  },
-  {
-    title: "MyLinks",
-    description:
-      "Site agregador de links que permite o compartilhamento de redes sociais, sites e outros links importantes em um único lugar.",
-    tags: ["Java", "Spark"],
-    imageURL: projectMyLinks,
-    demo: "https://eltonjcs.github.io/MyLinks",
-    repository: "https://github.com/EltonJCS/MyLinks",
-  },
-  {
-    title: "java_restAPI",
-    description:
-      "API REST em Java com funcionalidades de CRUD, utilizando o microframework Spark.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageURL: projectJavaRestAPI,
-    demo: "",
-    repository: "https://github.com/EltonJCS/java_restAPI_clientCRUD",
-  },
-] as const;
 
 const Projects = () => {
   const { ref } = useSectionInView("Projetos", 0.4);
