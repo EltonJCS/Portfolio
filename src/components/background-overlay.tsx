f5170b2a9319937c3176f95ac0e85c760aed1dde
EltonJCS <80626909+EltonJCS@users.noreply.github.com>
2024-01-30 23:55:04 -0300

    style: change BackgroundOverlay and add avatar-dark.png

--- src/components/background-overlay.tsx
+++ src/components/background-overlay.tsx
@@ -5,9 +5,9 @@ const BackgroundOverlay = () => {
   return (
     <>
       <motion.div
-        className="absolute right-0 top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-red-200 blur-[10rem] sm:w-[68.75rem]"
-        initial={{ opacity: 1, x: -100 }}
-        animate={{ opacity: 1, x: 0 }}
+        className="absolute right-0 top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-red-200 blur-[10rem] dark:bg-red-950 sm:w-[68.75rem]"
+        initial={{ opacity: 0.5, x: -50, y: 0 }}
+        animate={{ opacity: 1, x: 0, y: 100 }}
         transition={{
           delay: 0.5,
           duration: 2,
@@ -16,9 +16,9 @@ const BackgroundOverlay = () => {
         }}
       ></motion.div>
       <motion.div
-        className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-blue-200 blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"
-        initial={{ opacity: 1, x: 100 }}
-        animate={{ opacity: 1, x: 0 }}
+        className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-blue-200 blur-[10rem] dark:bg-blue-950 sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"
+        initial={{ opacity: 1, x: 0, y: 50 }}
+        animate={{ opacity: 0.5, x: 150, y: -50 }}
         transition={{
           delay: 0.5,
           duration: 2,
