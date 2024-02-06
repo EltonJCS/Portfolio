5d03cbbb81c7b6c960c0ec359c7b90437bc67a0d
EltonJCS <80626909+EltonJCS@users.noreply.github.com>
2024-02-05 23:54:38 -0300

    feat(hooks): implement a 3d hover effect hook

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,5 +1,5 @@
+import { useEffect, useRef, MouseEvent } from "react";
 import { useInView } from "react-intersection-observer";
-import { useEffect } from "react";
 
 import { useActiveSectionContext } from "../context/active-section-context";
 
@@ -19,3 +19,39 @@ export function useSectionInView(sectionName: SectionName, threshold = 0.8) {
     ref,
   };
 }
+
+// Add perspective to parent element and transform using the x and y variables
+export const use3DHoverEffect = (rotationIntensity: number | null) => {
+  const boundingRef = useRef<DOMRect | null>(null);
+
+  const handleMouseMove = (event: MouseEvent<HTMLElement>) => {
+    if (!boundingRef.current || !rotationIntensity) return;
+    const mouseX = event.clientX - boundingRef.current.left;
+    const mouseY = event.clientY - boundingRef.current.top;
+    const mouseXPercentage = mouseX / boundingRef.current.width;
+    const mouseYPercentage = mouseY / boundingRef.current.height;
+    const mouseXRotation = (mouseXPercentage - 0.5) * rotationIntensity;
+    const mouseYRotation = (0.5 - mouseYPercentage) * rotationIntensity;
+
+    event.currentTarget.style.setProperty(
+      "--x-rotation",
+      `${mouseYRotation}deg`,
+    );
+    event.currentTarget.style.setProperty(
+      "--y-rotation",
+      `${mouseXRotation}deg`,
+    );
+    event.currentTarget.style.setProperty("--x", `${mouseXPercentage * 100}%`);
+    event.currentTarget.style.setProperty("--y", `${mouseYPercentage * 100}%`);
+  };
+
+  const handleMouseLeave = () => {
+    boundingRef.current = null;
+  };
+
+  const handleMouseEnter = (event: MouseEvent<HTMLElement>) => {
+    boundingRef.current = event.currentTarget.getBoundingClientRect();
+  };
+
+  return { handleMouseMove, handleMouseLeave, handleMouseEnter };
+};
